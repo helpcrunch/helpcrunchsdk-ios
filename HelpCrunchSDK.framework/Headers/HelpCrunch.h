ae1ff7d16fa6f94323fbec9336b0067d087a71fe
Andrii Gusarov <fishkillerru@gmail.com>
2020-02-26 17:25:57 +0200

    2.0.11 Track event fix, stability improvements

--- HelpCrunchSDK.framework/Headers/HelpCrunch.h
+++ HelpCrunchSDK.framework/Headers/HelpCrunch.h
@@ -28,7 +28,7 @@ FOUNDATION_EXTERN NSString * _Nonnull const HCSUserClosedChatNotification;
  */
 FOUNDATION_EXTERN NSString * _Nonnull const HCSUnreadMessagesNotification;
 
-static NSString * _Nonnull const HCSSDKVersion = @"2.0.10";
+static NSString * _Nonnull const HCSSDKVersion = @"2.0.11";
 
 typedef void (^HCSCompletionHandler)(NSError * _Nullable error);
 
