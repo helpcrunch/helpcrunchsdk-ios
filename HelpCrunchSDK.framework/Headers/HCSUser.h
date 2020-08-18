8f2a7d348fd252bbe288822313e794cecac1268d
Andrii Gusarov <fishkillerru@gmail.com>
2020-08-19 01:18:06 +0300

    3.1.4

--- HelpCrunchSDK.framework/Headers/HCSUser.h
+++ HelpCrunchSDK.framework/Headers/HCSUser.h
@@ -4,11 +4,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface HCSUser : NSObject
 
-@property (nonatomic, strong) NSString *userId;
-@property (nonatomic, strong) NSString *name;
-@property (nonatomic, strong) NSString *email;
-@property (nonatomic, strong) NSString *phone;
-@property (nonatomic, strong) NSString *company;
+@property (nonatomic, strong, nullable) NSString *userId;
+@property (nonatomic, strong, nullable) NSString *name;
+@property (nonatomic, strong, nullable) NSString *email;
+@property (nonatomic, strong, nullable) NSString *phone;
+@property (nonatomic, strong, nullable) NSString *company;
 
 @property (nonatomic, strong) NSDictionary *customData;
 
