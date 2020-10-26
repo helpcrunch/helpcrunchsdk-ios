55d735a983f99f17d96520a97d0a19f65c357a79
Andrii Gusarov <fishkillerru@gmail.com>
2020-10-26 03:18:47 +0200

    3.1.8 - Fixed customer issue, updated to iOS 14

--- HelpCrunchSDK.framework/Headers/HelpCrunch.h
+++ HelpCrunchSDK.framework/Headers/HelpCrunch.h
@@ -59,7 +59,7 @@ typedef NS_ENUM(NSUInteger, HCSState) {
     HCSErrorState
 };
 
-static NSString * _Nonnull const HCSSDKVersion = @"3.1.7";
+static NSString * _Nonnull const HCSSDKVersion = @"3.1.8";
 
 typedef void (^HCSCompletionHandler)(NSError * _Nullable error);
 
