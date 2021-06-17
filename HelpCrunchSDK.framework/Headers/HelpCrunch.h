3aec119c324ca3a1fc8af9f7d5a6dabe52eb6a26
Andrii Gusarov <fishkillerru@gmail.com>
2021-06-17 19:19:07 +0300

    3.3.4

--- HelpCrunchSDK.framework/Headers/HelpCrunch.h
+++ HelpCrunchSDK.framework/Headers/HelpCrunch.h
@@ -59,7 +59,7 @@ typedef NS_ENUM(NSUInteger, HCSState) {
     HCSErrorState
 };
 
-static NSString * _Nonnull const HCSSDKVersion = @"3.3.3";
+static NSString * _Nonnull const HCSSDKVersion = @"3.3.4";
 
 typedef void (^HCSCompletionHandler)(NSError * _Nullable error);
 
