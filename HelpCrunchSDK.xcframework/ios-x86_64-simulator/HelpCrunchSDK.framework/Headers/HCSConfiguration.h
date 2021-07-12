506011a72c56fe48bf1be720609414bab252205c
Andrii Gusarov <fishkillerru@gmail.com>
2021-07-12 15:17:00 +0300

    Initial xcframework

---
+++ HelpCrunchSDK.xcframework/ios-x86_64-simulator/HelpCrunchSDK.framework/Headers/HCSConfiguration.h
@@ -0,0 +1,50 @@
+#import <Foundation/Foundation.h>
+#import "HCSUserAttribute.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HCSConfiguration : NSObject
+
+@property (nonatomic, strong, nullable) NSArray<HCSUserAttribute *> *userAttributes;
+
+@property (nonatomic, readonly) NSString *organization;
+@property (nonatomic, readonly) NSString *applicationId;
+@property (nonatomic, readonly) NSString *applicationSecret;
+
+@property (nonatomic) BOOL isLoggingEnabled;
+
+/**
+ Server-based properties.
+ */
+@property (nonatomic, readonly) BOOL isFileAttachmentAllowed;
+
+/**
+ @summary
+ If you're not using UserNotification delegate by yourself - set this property to True.
+ 
+ @discussion
+ If set to false - it's your responsibility to handle HelpCrunch push notificaitons.
+ Use this code:
+ \code
+ if ([HelpCrunch isHelpCrunchNotification:notification.request.content.userInfo]) {
+     if ([HelpCrunch isShowing]) {
+        completionHandler(UNNotificationPresentationOptionNone);
+     } else {
+        completionHandler(UNNotificationPresentationOptionAlert);
+     }
+ }
+ \endcode
+ */
+@property (nonatomic) BOOL shouldUsePushNotificationDelegate;
+
+
+/// Modal presentation style for chat view controller
+@property (nonatomic) UIModalPresentationStyle chatPresentationStyle;
+
++ (HCSConfiguration *)configurationForOrganization:(NSString *)organization
+                                     applicationId:(NSString *)applicationId
+                                 applicationSecret:(NSString *)applicationSecret;
+
+@end
+
+NS_ASSUME_NONNULL_END
