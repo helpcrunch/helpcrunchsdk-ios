b3b0ba814d2a33cdc9144c01ef31dc9ae461702a
Andrii Gusarov <fishkillerru@gmail.com>
2021-07-12 15:52:24 +0300

    targets

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "HelpCrunchSDK",
     products: [
-        .library(name: "HelpCrunchSDK", targets: ["HelpCrunchSDK, HelpCrunchSDKBinary"])
+        .library(name: "HelpCrunchSDK", targets: ["HelpCrunchSDK", "HelpCrunchSDKBinary"])
     ],
     dependencies: [    
     	.package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMinor(from: "15.0.0"))
