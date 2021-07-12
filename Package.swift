d89acfe074ae740f81ace9fc248a228614979afe
Andrii Gusarov <fishkillerru@gmail.com>
2021-07-12 20:47:00 +0300

    zip

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         //     path: "HelpCrunchSDK.xcframework"),
         .binaryTarget(
             name: "HelpCrunchSDKBinary",
-            url: "https://ucf0b01e0ff542ef7842e0adcd8b.dl.dropboxusercontent.com/cd/0/get/BSKH-xEwysJo737E6NVsYsePbrZ8JFLnytejDEWngXVYOMbGKXKbqTLFpT5NcJRfRrvjBGNDlenTqWhfNToEmeGOrUqBQSUR-pNUotgoUsmsw8-S7MJhY02Et-K5tyE0ASQ42Ou25tHWxlsI7TMGG0mQ/file?dl=1",
+            url: "https://sangwa.ru/HelpCrunchSDK.xcframework.zip",
             checksum: "699eb1be6c0cc030cc85274305cfd4e0eb5dd7fc5f7bed84029b2a4f8ba53f68"
             ),
         .target(
