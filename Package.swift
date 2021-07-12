5b7ff3beec60afb85411971a712c9049dcbb639f
Andrii Gusarov <fishkillerru@gmail.com>
2021-07-12 20:49:42 +0300

    test

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "HelpCrunchSDK",
     products: [
-        .library(name: "HelpCrunchSDK", targets: ["HelpCrunchSDK"])
+        .library(name: "HelpCrunchSDK", targets: ["HelpCrunchSDK", "SocketIO"])
     ],
     dependencies: [    
     	.package(name: "SocketIO", url: "https://github.com/socketio/socket.io-client-swift", .upToNextMajor(from: "15.0.0"))
@@ -14,12 +14,9 @@ let package = Package(
         //     name: "HelpCrunchSDKBinary", 
         //     path: "HelpCrunchSDK.xcframework"),
         .binaryTarget(
-            name: "HelpCrunchSDKBinary",
+            name: "HelpCrunchSDK",
             url: "https://sangwa.ru/HelpCrunchSDK.xcframework.zip",
             checksum: "699eb1be6c0cc030cc85274305cfd4e0eb5dd7fc5f7bed84029b2a4f8ba53f68"
             ),
-        .target(
-            name: "HelpCrunchSDK",
-            dependencies: ["HelpCrunchSDKBinary", "SocketIO"])
     ]
 )
