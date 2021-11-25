d654224120fc07c34371f055a276055dba1dc924
Andrii Gusarov <fishkillerru@gmail.com>
2021-11-25 16:49:36 +0200

    Fixed rare bug on automessages

--- HelpCrunchSDK.xcframework/ios-arm64_i386_x86_64-simulator/HelpCrunchSDK.framework/Headers/HelpCrunch.h
+++ HelpCrunchSDK.xcframework/ios-arm64_i386_x86_64-simulator/HelpCrunchSDK.framework/Headers/HelpCrunch.h
@@ -59,7 +59,7 @@ typedef NS_ENUM(NSUInteger, HCSState) {
     HCSErrorState
 };
 
-static NSString * _Nonnull const HCSSDKVersion = @"4.1.0";
+static NSString * _Nonnull const HCSSDKVersion = @"4.1.1";
 
 typedef void (^HCSCompletionHandler)(NSError * _Nullable error);
 
