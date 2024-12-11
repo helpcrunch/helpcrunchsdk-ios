7b8b94f091b0e97003342bb63afb38955ae1d2f2
Andrii Gusarov <fishkillerru@gmail.com>
2024-12-11 15:13:38 +0200

    Added Privacy manifset

--- HelpCrunchSDK.xcframework/ios-arm64/HelpCrunchSDK.framework/Headers/HelpCrunch.h
+++ HelpCrunchSDK.xcframework/ios-arm64/HelpCrunchSDK.framework/Headers/HelpCrunch.h
@@ -63,7 +63,7 @@ typedef NS_ENUM(NSUInteger, HCSState) {
     HCSHiddenState
 };
 
-static NSString * _Nonnull const HCSSDKVersion = @"4.4.5";
+static NSString * _Nonnull const HCSSDKVersion = @"4.4.6";
 
 typedef void (^HCSCompletionHandler)(NSError * _Nullable error);
 
