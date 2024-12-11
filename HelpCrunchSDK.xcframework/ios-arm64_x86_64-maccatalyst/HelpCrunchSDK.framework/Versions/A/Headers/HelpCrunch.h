a2580637c37481483fa3582b644febc7a82e9e1f
Andrii Gusarov <fishkillerru@gmail.com>
2024-12-11 16:56:56 +0200

    4.4.7 - Updates in Privacy Manifest

--- HelpCrunchSDK.xcframework/ios-arm64_x86_64-maccatalyst/HelpCrunchSDK.framework/Versions/A/Headers/HelpCrunch.h
+++ HelpCrunchSDK.xcframework/ios-arm64_x86_64-maccatalyst/HelpCrunchSDK.framework/Versions/A/Headers/HelpCrunch.h
@@ -63,7 +63,7 @@ typedef NS_ENUM(NSUInteger, HCSState) {
     HCSHiddenState
 };
 
-static NSString * _Nonnull const HCSSDKVersion = @"4.4.6";
+static NSString * _Nonnull const HCSSDKVersion = @"4.4.7";
 
 typedef void (^HCSCompletionHandler)(NSError * _Nullable error);
 
