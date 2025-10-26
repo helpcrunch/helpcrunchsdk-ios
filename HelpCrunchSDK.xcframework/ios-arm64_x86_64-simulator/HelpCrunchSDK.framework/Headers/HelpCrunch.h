db817a3b600741e7e428b089a2f9e13ef01b8c6a
Andrii Gusarov <3305980+Fishkiller@users.noreply.github.com>
2025-10-26 21:32:12 +0200

    4.5.2

--- HelpCrunchSDK.xcframework/ios-arm64_x86_64-simulator/HelpCrunchSDK.framework/Headers/HelpCrunch.h
+++ HelpCrunchSDK.xcframework/ios-arm64_x86_64-simulator/HelpCrunchSDK.framework/Headers/HelpCrunch.h
@@ -63,7 +63,7 @@ typedef NS_ENUM(NSUInteger, HCSState) {
     HCSHiddenState
 };
 
-static NSString * _Nonnull const HCSSDKVersion = @"4.5.1";
+static NSString * _Nonnull const HCSSDKVersion = @"4.5.2";
 
 typedef void (^HCSCompletionHandler)(NSError * _Nullable error);
 
